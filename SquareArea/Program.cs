fe8d365df411e73a5c5e42a2614763ddfd408b6f
estefivilla07@gmail.com <estefivilla07@gmail.com>
2024-02-27 21:09:07 -0600

    Agregar archivos de proyecto.

---
+++ SquareArea/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Drawing;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter the length of the side of the square: ");
+        double sidelength = Convert.ToDouble(Console.ReadLine());
+
+        
+        Square miSquare = new Square(sidelength);
+
+        
+        Console.WriteLine($"Area of ​​the square: {mySquare.CalculateArea()}");
+
+        
+        Console.Write("Enter the width of the rectangle: ");
+        double rectanglewidth = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine($"Área del rectángulo: {mySquare.CalcularArea(rectanglewidth)}");
+    }
+}
+
---
+++ SquareArea/Square.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquareArea
+{
+
+namespace CalculationAreaSquare
+    {
+        class Square
+        {
+            private double side;
+
+           
+            public Square(double l)
+            {
+                side = l;
+            }
+
+            
+            public double CalculateArea()
+            {
+                return side * side;
+            }
+            
+            public double CalculateArea(double broad)
+            {
+                return side * broad;
+            }
+        }
+    }
+}
